d6fb75cd543c621b91e17845a5962cdd33158fba
Milan Rusimov <rusmil89@gmail.com>
2026-01-09 21:02:39 +0100

    [SA-7] Add MainMenuController
    Context:
    And new scenes, each representing a separate task

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,9 +1,43 @@
+using Cysharp.Threading.Tasks;
+using Softgames.Core;
+using Softgames.Core.Services;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Softgames.MainMenu
 {
 	public class MainMenuController : MonoBehaviour
 	{
+		[Header("Level Buttons")]
+		[SerializeField] private Button _task1Button;
+		[SerializeField] private Button _task2Button;
+		[SerializeField] private Button _task3Button;
 		
+		private ISceneLoaderService _sceneLoaderService;
+
+		//-----------------------------------------------------------------------
+		private void Start()
+		{
+			_sceneLoaderService = ServiceLocator.GetService<SceneLoaderService>();
+			RegisterButtonListeners();
+		}
+
+		//-----------------------------------------------------------------------
+		private void RegisterButtonListeners()
+		{
+			_task1Button.onClick.AddListener(() => LoadScene("AceOfShadows"));
+			_task2Button.onClick.AddListener(() => LoadScene("MagicWords"));
+			_task3Button.onClick.AddListener(() => LoadScene("PhoenixFlame"));
+		}
+
+		//-----------------------------------------------------------------------
+		private void LoadScene(string sceneName)
+		{
+			_task1Button.interactable = false;
+			_task2Button.interactable = false;
+			_task3Button.interactable = false;
+			
+			_sceneLoaderService.LoadSceneAsync(sceneName).Forget();
+		}
 	}
 }
