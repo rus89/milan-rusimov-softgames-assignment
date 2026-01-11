4dfc6038a54ec62e8f9fe87b3f86d2dbc9152551
Milan Rusimov <rusmil89@gmail.com>
2026-01-11 16:58:14 +0100

    Remove IGameService
    Not used

--- Assets/Scripts/Core/Services/IGameService.cs
+++
@@ -1,9 +0,0 @@
-using Cysharp.Threading.Tasks;
-
-namespace Softgames.Core.Services
-{
-	public interface IGameService
-	{
-		public UniTask InitializeAsync();
-	}
-}
--- Assets/Scripts/Core/Services/ISceneLoaderService.cs
+++ Assets/Scripts/Core/Services/ISceneLoaderService.cs
@@ -2,7 +2,7 @@ using Cysharp.Threading.Tasks;
 
 namespace Softgames.Core.Services
 {
-	public interface ISceneLoaderService : IGameService
+	public interface ISceneLoaderService
 	{
 		 public UniTask LoadSceneAsync(string sceneName, bool additive = false);
 	}
